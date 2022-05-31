9b76cfb175e9c97b6c65e8849ff9808aabfba6f9
mavan <mavan@DESKTOP-1CHLP67>
2022-05-31 16:00:10 +0530

    added feature2

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,7 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
-//		added feature 1
+//		added feature 2
 		SpringApplication.run(PetClinicApplication.class, args);
 //      modified other changes
 	}
